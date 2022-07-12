844fec039606ab0b933639dd6f86867d219dcf8b
rafterco <colinrafter82@gmail.com>
2022-07-12 15:17:05 +0100

    Initial commit

---
+++ src/test/java/com/betterreads/data/loader/BetterReadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betterreads.data.loader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterReadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
